71b5c23af96552de37c9e5088a506cba3bf0d180
Joysten28 <joysten.23cs050@sode-edu.in>
2024-11-05 15:53:43 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+
+public class M7 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        System.out.println(t1.reverse("Joysten"));
+    }
+    
+}
