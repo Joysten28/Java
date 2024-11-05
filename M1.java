dd061dfbee4757bd97168f9ccee49a6398b301cc
Joysten28 <joysten.23cs050@sode-edu.in>
2024-11-05 15:50:42 +0530

    Add files via upload

---
+++ M1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+public class M1 {
+   public static void main(String[] args) {
+       for(int i=0;i<=4;i++){
+           System.out.println(2*i);
+       }
+       for(int i=0;i<9;i=i+2){
+          System.out.println(3+i); 
+       }
+       for
+       
+       
+    }
+}
