379aba8858ae834af8cd391b914c0e479d25c819
Joysten28 <joysten.23cs050@sode-edu.in>
2024-11-06 20:33:17 +0530

    Update M7.java

--- M7.java
+++ M7.java
@@ -8,6 +8,20 @@
  *
  * @author smv
  */
+package oopj1;
+import oopj1.Tools1;
+class Tools1{
+    String s1="";
+    String s2="";
+    int len1=0;
+    public String reverse(String s1){
+        len1=s1.length();
+        for(int i=0;i<len1;i++){
+            s2=s2+s1.substring(len1-(i+1),len1-i);
+        }
+        return s2;
+    }
+}
 
 public class M7 {
     public static void main(String[] args){
