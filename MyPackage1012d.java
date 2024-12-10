991c206bece05879798c7df17fa5f335640fed9a
Joysten28 <joysten.23cs050@sode-edu.in>
2024-12-10 15:16:55 +0530

    Add files via upload

---
+++ MyPackage1012d.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3;
+
+/**
+ *
+ * @author smv
+ */
+public class MyPackage1012d {
+     public void display(){
+        System.out.println("This mag is from MyPackage class within pkg");
+    }
+}
