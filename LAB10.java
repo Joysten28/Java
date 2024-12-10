5a96bc74f3e8a130cc5c767ed75ce1c2adb815b5
Joysten28 <joysten.23cs050@sode-edu.in>
2024-12-10 15:16:02 +0530

    Add files via upload

---
+++ LAB10.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+import pkg3.MyPackage1012d;
+public class LAB10 {
+    public static void main(String[] args){
+        System.out.println("Package");
+        MyPackage1012d mypack1=new MyPackage1012d();
+        mypack1.display();
+    }
+    
+}
